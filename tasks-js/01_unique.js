60a04213e6540f05991acb05ac9fae3a44e17529
kuzmrom7 <kuzmrom7@gmail.com>
2022-04-22 11:26:59 +0300

    initial

---
+++ tasks-js/01_unique.js
@@ -0,0 +1,23 @@
+function isUnique(str) {
+  for (i = 0; i < str.length; i++) {
+    const value = str[i];
+
+    if (str.lastIndexOf(value) !== i) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Used Set
+
+// function isUnique(str) {
+//   return new Set(str).size === str.length;
+// }
+
+console.log(isUnique('abcdef')) // -> true
+console.log(isUnique('1234567')) // -> true
+console.log(isUnique('abcABC')) // -> true
+console.log(isUnique('abcadef')) // -> false
+
