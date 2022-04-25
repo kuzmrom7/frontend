cc96a76ba72da64b6620e8cb1372c2975ee414ba
kuzmrom7 <kuzmrom7@gmail.com>
2022-04-25 16:40:12 +0300

    add task

---
+++ tasks-js/09_search.js
@@ -0,0 +1,37 @@
+/* Solution */
+function search(array, target) {
+  let start = 0
+  let end = array.length - 1
+
+  if (target < array[start] || target > array[end]) {
+    return -1
+  }
+
+  while (true) {
+    if (target === array[start]) {
+      return start
+    }
+
+    if (target === array[end]) {
+      return end
+    }
+
+    if (end - start <= 1) {
+      return -1
+    }
+
+    const middle = Math.floor((start + end) / 2)
+
+    if (target > array[middle]) {
+      start = middle + 1
+    } else if (target < array[middle]) {
+      end = middle - 1
+    } else {
+      return middle
+    }
+  }
+}
+
+/* Test */
+console.log(search([1, 3, 6, 13, 17], 13)) // -> 3
+console.log(search([1, 3, 6, 13, 17], 12)) // -> -1
---
+++ tasks-js/10_balance.js
@@ -0,0 +1,34 @@
+/* Solution */
+function isBalanced(str) {
+  const opened = '[{(';
+  const closed = ')}]';
+  const obj = {
+    "]": "[",
+    ")": "(",
+    "}": "{",
+  }
+  const arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (opened.includes(char)) {
+      arr.push(char);
+    }
+    if (closed.includes(char)) {
+      let last = arr.pop();
+      if (obj[char] !== last) {
+        return false;
+      }
+    }
+  }
+
+  return !arr.length;
+}
+
+/* Test */
+console.log(isBalanced('(x + y) - (4)')) // -> true
+console.log(isBalanced('(((10 ) ()) ((?)(:)))')) // -> true
+console.log(isBalanced('[{()}]')) // -> true
+console.log(isBalanced('(50)(')) // -> false
+console.log(isBalanced('[{]}')) // -> false
