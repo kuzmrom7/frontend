d4bb949931e41ff47b5baaa6d7e76ed7a744045d
kuzmrom7 <kuzmrom7@gmail.com>
2022-04-25 11:28:29 +0300

    add leetcode 535 problem

---
+++ leetcode/535_encode_and_decode_tiny_url.js
@@ -0,0 +1,49 @@
+const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHUJKLMNOPQRSTUVWXYZ';
+const url = 'https://tinyurl.com/';
+
+const codeMap = new Map();
+const urlMap = new Map();
+
+function generateCode() {
+  return url + new Array(6)
+    .fill("")
+    .map(_ => chars.charAt(~~(Math.random() * 62)))
+    .join("")
+}
+
+/**
+ * Encodes a URL to a shortened URL.
+ */
+function encode(longUrl) {
+  if (urlMap.has(longUrl)) {
+    return urlMap.get(longUrl);
+  }
+
+  let code = generateCode();
+
+  codeMap.set(code, longUrl)
+  urlMap.set(longUrl, code)
+
+  return code;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ */
+function decode(shortUrl) {
+  return codeMap.get(shortUrl);
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
+
+
+/**
+ * Test
+ */
+
+// let tiny = encode("https://leetcode.com/problems/design-tinyurl");
+// let long = decode(tiny);
+// console.log({ tiny, long })
---
+++ tasks-js/08_matrix.js
@@ -0,0 +1,11 @@
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+function rotate(source) {
+  // todo
+}
+
+console.log(rotate(matrix))
