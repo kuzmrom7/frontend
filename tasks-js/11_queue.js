9d64913636fff63080f02f7024efee9f3ceca6e7
kuzmrom7 <kuzmrom7@gmail.com>
2022-04-25 19:04:48 +0300

    add task queue

---
+++ tasks-js/11_queue.js
@@ -0,0 +1,48 @@
+/* Solution */
+class Queue {
+  #storage = {}
+  #last = 0
+  #first = 0
+
+  get size() {
+    return this.#last - this.#first;
+  }
+
+  print() {
+    Object.keys(this.#storage).forEach(id => {
+      const element = this.#storage[id];
+      console.log({ id, element })
+    })
+  }
+
+  add(item) {
+    this.#storage[this.#last] = item;
+    this.#last++;
+  }
+
+  delete() {
+    if (this.size === 0) {
+      return null;
+    }
+
+    const value = this.#storage[this.#first];
+
+    delete this.#storage[this.#first];
+    this.#first++;
+
+    return value;
+  }
+}
+
+/* Test */
+const queue1 = new Queue();
+
+queue1.add("hello");
+queue1.add("world");
+queue1.add([]);
+queue1.delete();
+
+console.log(queue1.size);
+queue1.print();
+
+
