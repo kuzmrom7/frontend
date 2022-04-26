1fca79c7d9cb1b9779bce7220319f3cb7de2cc6f
kuzmrom7 <kuzmrom7@gmail.com>
2022-04-26 12:22:06 +0300

    add leetcode task

---
+++ leetcode/1_two_sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+  const seen = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    let diff = target - num;
+
+    if (seen.has(diff)) {
+      return [seen.get(diff), i];
+    }
+
+    seen.set(num, i);
+  }
+
+  return [];
+}
+
+
+/**
+ * Test
+ */
+
+// console.log(twoSum([2, 7, 11, 15], 9)); // -> [0, 1]
