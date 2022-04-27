4b19b371705ba5c5f8e08059cfc20e3d8e16cd0f
kuzmrom7 <kuzmrom7@gmail.com>
2022-04-27 16:49:53 +0300

    add leetcode 268

---
+++ leetcode/268_missing_number.js
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/missing-number/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function missingNumber(nums) {
+  let n = nums.length;
+  let sum = nums.reduce((acc, curr) => acc + curr, 0)
+
+  return n * (n + 1) / 2 - sum;
+};
+
+/* Test */
+// console.log(missingNumber([3, 0, 1])); // -> 2
+// console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // -> 8
+// console.log(missingNumber([0, 1])); // -> 2
