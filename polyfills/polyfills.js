6407c64dbb09ffa4c27ec073d75aed4066bb384a
kuzmrom7 <kuzmrom7@gmail.com>
2022-05-11 12:39:48 +0500

    add reduce

--- polyfills/polyfills.js
+++ polyfills/polyfills.js
@@ -21,5 +21,24 @@ module.exports = function () {
   }
 
   // .reduce
+  if (!Array.prototype.myReduce) {
+    Array.prototype.myReduce = function (cb, initial) {
+      if (!(this instanceof Array || this instanceof String)) {
+        throw new TypeError('Array.prototype.myReduce was called on wrong type.')
+      }
 
+      if (typeof cb !== 'function') {
+        throw new TypeError(`Array.prototype.myReduce ${cb} is not a function.`)
+      }
+
+      let acc = arguments.length >= 2 ? initial : this[0]
+      let start = arguments.length >= 2 ? 0 : 1
+
+      for (let i = start; i < this.length; i++) {
+        acc = cb(acc, this[i], i, this)
+      }
+
+      return acc;
+    }
+  }
 }
--- polyfills/polyfills.test.js
+++ polyfills/polyfills.test.js
@@ -1,6 +1,5 @@
 const polyfills = require('../polyfills')
 
-
 describe('Custom polyfills testing', () => {
   beforeAll(() => {
     polyfills()
@@ -10,39 +9,91 @@ describe('Custom polyfills testing', () => {
     it('Check — wrong type', () => {
       function badFn() {
         Array.prototype.myMap.call(true, (value) => value)
-      };
-      expect(badFn).toThrow(TypeError);
-    });
+      }
+      expect(badFn).toThrow(TypeError)
+    })
 
     it('Check — no callback', () => {
       function badFn() {
-        [1, 2, 3].myMap();
-      };
-      expect(badFn).toThrow(TypeError);
-    });
+        [1, 2, 3].myMap()
+      }
+      expect(badFn).toThrow(TypeError)
+    })
 
     it('Check — simple loop', () => {
       expect(
         [1, 2, 3, 4].myMap((item) => item + 10)
       ).toEqual(
         [1, 2, 3, 4].map((item) => item + 10)
-      );
-    });
+      )
+    })
 
     it('Check — string', () => {
       expect(
         Array.prototype.myMap.call('test', (value) => `${value},`)
       ).toEqual(
         Array.prototype.map.call('test', (value) => `${value},`)
-      );
-    });
+      )
+    })
 
     it('Check — properties', () => {
-      const arr = [1, 2, 3, 4];
+      const arr = [1, 2, 3, 4]
       expect(arr.myMap((item, index, arr) => [item, index, arr])).toEqual(
         arr.map((item, index, arr) => [item, index, arr])
-      );
-    });
+      )
+    })
   })
 
+  describe('Testing myReduce', () => {
+    it('Check — wrong type', () => {
+      function badFn() {
+        Array.prototype.myReduce.call(true, (acc, value) => acc + value)
+      }
+      expect(badFn).toThrow(TypeError)
+    })
+
+    it('Check — no callback', () => {
+      function badFn() {
+        [1, 2, 3].myReduce()
+      }
+      expect(badFn).toThrow(TypeError)
+    })
+
+    it('Check — sum', () => {
+      const arr = [1, 2, 3, 4]
+      expect(
+        arr.myReduce((acc, value) => acc + value)
+      ).toEqual(
+        arr.reduce((acc, value) => acc + value)
+      )
+    })
+
+    it('Check — string', () => {
+      expect(
+        Array.prototype.myReduce.call('test', (acc, value) => {
+          acc[value] = value + 1
+          return acc
+        }, {})
+      ).toEqual(
+        Array.prototype.reduce.call('test', (acc, value) => {
+          acc[value] = value + 1
+          return acc
+        }, {})
+      )
+    })
+
+    it('Check — properties', () => {
+      expect(
+        [1, 2, 3, 4].myReduce((acc, value, index, arr) => {
+          acc.push([value, index, arr])
+          return acc
+        }, [])
+      ).toEqual(
+        [1, 2, 3, 4].reduce((acc, value, index, arr) => {
+          acc.push([value, index, arr])
+          return acc
+        }, [])
+      )
+    })
+  })
 })
