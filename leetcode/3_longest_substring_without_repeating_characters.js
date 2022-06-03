c8bdd4fa8c6eb9a680023be8cfd5e116f56bdb99
kuzmrom7 <kuzmrom7@gmail.com>
2022-06-03 08:27:05 +0500

    add leetcode 3 solution

---
+++ leetcode/3_longest_substring_without_repeating_characters.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+/**
+ * @param {string} str
+ * @return {number}
+ */
+function lengthOfLongestSubstring(str) {
+  let maxLength = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const currentSet = new Set();
+
+    for (let j = i; j < str.length; j++) {
+      let currentJ = str[j];
+
+      if (currentSet.has(currentJ)) {
+        break;
+      } else {
+        currentSet.add(currentJ);
+      }
+    }
+
+    maxLength = Math.max(maxLength, currentSet.size);
+  }
+
+  return maxLength;
+};
+
+
+
+
+// Test
+console.log(lengthOfLongestSubstring('abcabcbb')) // => 3
+console.log(lengthOfLongestSubstring('bbbbb')) // => 1
+console.log(lengthOfLongestSubstring('pwwkew')) // =>3
